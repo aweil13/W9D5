217fb21b99fc46d285d6e4fa8ccdbf2f54e054de
Kevin <kevinmao.xy@gmail.com>
2021-02-26 10:51:56 -0500

    add links

--- skeleton/src/drop_down.js
+++ skeleton/src/drop_down.js
@@ -9,3 +9,27 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+const dogLinkCreator = () => {
+  const dogLinks = [];
+
+  for (const [key, value] of Object.entries(dogs)) {
+    const a = document.createElement("a");
+    a.innerHTML = key;
+    a.href = value;
+    const li = document.createElement("li");
+    li.className = "dog-link";
+    li.appendChild(a);
+    dogLinks.push(li);
+  }
+
+  return dogLinks;
+}
+
+export const attachDogLinks = () => {
+  const dogLinks = dogLinkCreator();
+  dogLinks.forEach( el => {
+    document.getElementsByClassName("drop-down-dog-list")[0].appendChild(el);
+  });
+}
+
+attachDogLinks();
