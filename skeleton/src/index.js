19f9455a4a088ea5cda9bfcf866ed81c31b4ca52
alejandro <alejandroweil13@gmail.com>
2021-02-26 14:39:59 -0500

    Starting phase 4

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,3 +1,4 @@
 import warmUp from "./warmup";
 import Clock from "./clock"
 import DogLinks from "./drop_down"
+import Todo from "./todo_list"
--- skeleton/src/todo_list.js
+++ skeleton/src/todo_list.js
@@ -1,4 +1,5 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
 
 const ulTodos = document.querySelector('.todos');
 const formTodos = document.querySelector('.add-todo-form');
@@ -15,4 +16,37 @@ const addTodos = (e) => {
     }
 
     todos.push(todo)
+    populateList(todos);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+const populateList = (arr) => {
+    arr.forEach((ele, i) => {
+        const label = document.createElement('label');
+        label.textContent = ele.inputVal;
+        const input = document.createElement('input');
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("data-idx", `${i}` )
+        if (ele.done) {
+            input.checked = true;
+        }
+        label.appendChild(input);
+        const li = document.createElement("li");
+        li.appendChild(label);
+        document.querySelector(".todos").appendChild(li);
+    });
+} 
+
+const toggleCheck = (e) => {
+    const element = e.target;
+    const index = element.dataset.idx;
+    console.log(index)
+    todos[index].done = !todos[index].done;
+    localStorage.setItem('todos', JSON.stringify(todos));
+    // populateList(todos);
+}
+
+
+formTodos.addEventListener('submit', addTodos);
+ulTodos.addEventListener('click', toggleCheck);
+populateList(todos);
